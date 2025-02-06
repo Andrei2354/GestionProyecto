ed73700fde5949135f3451c0311bd5bbcbf19465
Andrei2354 <andreiaimar1362@gmail.com>
2025-02-06 08:39:21 +0000

    Updated y creado

--- src/main/kotlin/network/LoginApi.kt
+++ src/main/kotlin/network/LoginApi.kt
@@ -1,20 +1,28 @@
-import io.ktor.client.utils.EmptyContent.contentType
+package network
+
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import modelo.LoginRequest
+import modelo.User
+import network.NetworkUntils.httpClient
+import utils.sha512
 
-fun apiLogIn(usuario:String, password: String, OnSuccessrResponse: (User) -> Unit){
-    val url = "http://127.0.0.1:500/gestor/login"
+fun apiLogIn(usuario: String, password: String, onSuccessResponse: (User) -> Unit){
+    val url = "http://127.0.0.1:5000/login_user"
     CoroutineScope(Dispatchers.IO).launch {
         val response = httpClient.post(url){
             contentType(ContentType.Application.Json)
             setBody(LoginRequest(usuario, sha512(password)))
         }
-        if (respone.status == HttpStatusCode.Ok){
+        if (response.status == HttpStatusCode.OK){
             val user = response.body<User>()
-            onSuccesResponse(user)
-        } else {
+            onSuccessResponse(user)
+        } else{
             println("Error: ${response.status}, Body: ${response.bodyAsText()}")
         }
     }
